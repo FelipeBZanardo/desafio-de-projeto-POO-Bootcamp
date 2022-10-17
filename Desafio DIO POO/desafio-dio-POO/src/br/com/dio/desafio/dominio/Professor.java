dce32b8271343713e5e031816333636bb0cfb0d0
FelipeBZanardo <felipebueno6@hotmail.com>
2022-10-17 17:56:02 -0300

    criado a Classe Professor

---
+++ Desafio DIO POO/desafio-dio-POO/src/br/com/dio/desafio/dominio/Professor.java
@@ -0,0 +1,17 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Professor {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
